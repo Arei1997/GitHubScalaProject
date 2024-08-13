c4b43883f00a5e87aa0559aaa3487d906b089a79
Joe Higgs <joe.higgs@mercator.group>
2024-08-13 14:13:27 +0100

    Remove html_url and replace with url

--- app/model/Repository.scala
+++ app/model/Repository.scala
@@ -4,7 +4,7 @@ import play.api.libs.json.{Json, OFormat}
 
 case class Repository(
                        name: String,
-                       html_url: String,
+                       url: String,
                        description: Option[String]
                      )
 
