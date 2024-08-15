d108279ac37d73b25f3f1c732697f09bf66edb3e
Joe Higgs <joe.higgs@mercator.group>
2024-08-15 12:35:35 +0100

    Update route

--- app/controllers/GitHubReposController.scala
+++ app/controllers/GitHubReposController.scala
@@ -1,22 +1,22 @@
 package controllers
 
-import model.{APIError, Repository, User}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
-import play.api.libs.json.{JsError, JsSuccess, JsValue, Json}
-import repository.DataRepository
-import service.{GitHubService, RepositoryService}
-
+import cats.data.EitherT
+import connector.GitHubConnector
+import model.APIError
+import play.api.mvc.{AbstractController, Action, AnyContent, BaseController, ControllerComponents, Request}
+import play.api.libs.json.JsValue
+import model.Contents
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class GitHubReposController @Inject()(
-                                       val controllerComponents: ControllerComponents,
-                                       dataRepository: DataRepository,
-                                       githubService: GitHubService,
-                                       repositoryService: RepositoryService
-                                     )(implicit ec: ExecutionContext) extends BaseController with play.api.i18n.I18nSupport {
+class GitHubReposController @Inject()(gitHubConnector: GitHubConnector, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+
+  def readFile(username: String, repoName: String, path: String): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
+    val url = s"https://api.github.com/repos/$username/$repoName/contents/$path"
+
+    val response: EitherT[Future, APIError, JsValue] = gitHubConnector.get[Contents](url)
 
 
 
-}
+  }
