126fbf036f10acafce0a4431e3da2df744def248
Arei Mohammed <Arei1997@hotmail.co.uk>
2024-08-18 22:50:18 +0100

    d

--- app/controllers/GitHubReposController.scala
+++ app/controllers/GitHubReposController.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import model.{APIError, Contents, Delete, FileFormData}
-
 import play.api.i18n.Messages
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
@@ -12,7 +11,7 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class GitHubReposController @Inject()(repositoryService: RepositoryService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController   (cc) with play.api.i18n.I18nSupport  {
+class GitHubReposController @Inject()(repositoryService: RepositoryService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
   def readFile(username: String, repoName: String, path: String): Action[AnyContent] = Action.async { implicit request =>
     repositoryService.getFileContent(username, repoName, path).value.map {
@@ -42,14 +41,9 @@ class GitHubReposController @Inject()(repositoryService: RepositoryService, cc: 
     }
   }
 
-  import model.FileFormData
-
   def createFileForm(username: String, repoName: String, path: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.fileForm(FileFormData.form, username, repoName, path))
   }
-
-
-
 
   def submitFileForm(username: String, repoName: String, path: String): Action[AnyContent] = Action.async { implicit request =>
     FileFormData.form.bindFromRequest().fold(
@@ -57,35 +51,29 @@ class GitHubReposController @Inject()(repositoryService: RepositoryService, cc: 
         Future.successful(BadRequest(views.html.fileForm(formWithErrors, username, repoName, path)))
       },
       data => {
-        repositoryService.getFileContent(username, repoName, path).value.flatMap {
+        val fileName = if (path.isEmpty || path == "root") {
+          data.fileName
+        } else {
+          path
+        }
+
+        repositoryService.getFileContent(username, repoName, fileName).value.flatMap {
           case Right(existingFile) =>
             // Update the file if it exists
-            repositoryService.createOrUpdateFile(username, repoName, path, data.message, data.content, Some(existingFile.sha)).value.map {
-              case Right(_) => Ok(views.html.fileForm(FileFormData.form.fill(data), username, repoName, path)).flashing("success" -> "File updated successfully")
-              case Left(error) => BadRequest(views.html.fileForm(FileFormData.form.withError("error", error.reason), username, repoName, path))
+            repositoryService.createOrUpdateFile(username, repoName, fileName, data.message, data.content, Some(existingFile.sha)).value.map {
+              case Right(_) => Ok(views.html.fileForm(FileFormData.form.fill(data), username, repoName, fileName)).flashing("success" -> "File updated successfully")
+              case Left(error) => BadRequest(views.html.fileForm(FileFormData.form.withError("error", error.reason), username, repoName, fileName))
             }
           case Left(_) =>
             // Create a new file if it doesn't exist
-            repositoryService.createOrUpdateFile(username, repoName, path, data.message, data.content, None).value.map {
-              case Right(_) => Ok(views.html.fileForm(FileFormData.form.fill(data), username, repoName, path)).flashing("success" -> "File created successfully")
-              case Left(error) => BadRequest(views.html.fileForm(FileFormData.form.withError("error", error.reason), username, repoName, path))
+            repositoryService.createOrUpdateFile(username, repoName, fileName, data.message, data.content, None).value.map {
+              case Right(_) => Ok(views.html.fileForm(FileFormData.form.fill(data), username, repoName, fileName)).flashing("success" -> "File created successfully")
+              case Left(error) => BadRequest(views.html.fileForm(FileFormData.form.withError("error", error.reason), username, repoName, fileName))
             }
         }
       }
     )
   }
-
-
-
-
-
-
-
-
-
-
-
-
 
   def deleteFileForm(username: String, repoName: String, path: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.deleteFileForm(Delete.form, username, repoName, path))
@@ -104,16 +92,4 @@ class GitHubReposController @Inject()(repositoryService: RepositoryService, cc: 
       }
     )
   }
-
-
-
-
-
-
-  private def accessToken(implicit request: Request[_]) = {
-    CSRF.getToken
-  }
-
-
 }
-
--- app/model/FileFormData.scala
+++ app/model/FileFormData.scala
@@ -3,11 +3,12 @@ package model
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class FileFormData(message: String, content: String, sha: Option[String])
+case class FileFormData(fileName: String, message: String, content: String, sha: Option[String])
 
 object FileFormData {
   val form: Form[FileFormData] = Form(
     mapping(
+      "fileName" -> nonEmptyText,
       "message" -> nonEmptyText,
       "content" -> nonEmptyText,
       "sha" -> optional(text)
